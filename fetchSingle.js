858a08205d9b3f79c97ff1404ca5ace8aae6fa20
Artur Iatsko <arturiatsko@Arturs-MBP.hitronhub.home>
2020-04-28 13:28:46 -0700

    Individual fetch working

--- fetchSingle.js
+++ fetchSingle.js
@@ -20,7 +20,27 @@ const input = [
 const axios = require("axios");
 const cheerio = require("cheerio");
 
+async function fetchData(url) {
+  // make http call to url
+  let response = await axios(url).catch((err) => console.log(err));
+
+  if (response.status !== 200) {
+    console.log("Error occurred while fetching data");
+    return;
+  }
+  return response;
+}
+
 input.forEach((element) => {
   const address = element.url;
-  console.log(address);
+  fetchData(address).then((res) => {
+    const html = res.data;
+    const $ = cheerio.load(html);
+    const statsTable = $(".offers");
+    statsTable.each(function () {
+      console.log(`Data for ${address}`);
+      let online = $(this).find("span").text();
+      console.log(online);
+    });
+  });
 });
